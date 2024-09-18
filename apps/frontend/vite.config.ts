1f5260aafe1c6774953853bb85d05943e6c5f2a9
Robin <git+r@obin.ch>
2024-09-18 12:52:26 +0000

    chore: configure pnpm workspaces

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001
+  },
+  clearScreen: false,
   plugins: [react()],
 })
