d4d820377aa743f46d37bdf497f9e42a30a51f0b
Robin <git+r@obin.ch>
2024-09-30 11:08:48 +0000

    chore: add project colors

--- libs/ui/src/theme/Theme.ts
+++ libs/ui/src/theme/Theme.ts
@@ -3,7 +3,7 @@ import "./Theme.css";
 
 export const theme: MantineThemeOverride = {
   colors: {
-    orange: ["#FFECE5", "#FFC9B8", "#FFA68A", "#FF835C", "#FF602E", "#FF3D00", "#CC3100", "#992500", "#661800", "#330C00"],
+    orange: ["#fff0e4", "#fee1d1", "#f4c1a6", "#eca077", "#e6844f", "#e27135", "#e16827", "#c8571a", "#b24c14", "#9c3f0b"],
   },
   primaryColor: "orange",
 };
