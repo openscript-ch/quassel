2b2019574fa7c143c2f2a4dc319e52067234bb73
stampaaaron <aaron@stampa.ch>
2024-09-30 12:32:13 +0000

    feat(frontend): setup api client generation

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "@quassel/ui/style.css";
 import { ThemeProvider } from "@quassel/ui";
+import { ApiProvider } from "./provider/ApiProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <ApiProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ApiProvider>
   </StrictMode>
 );
