c877b1fa3fc4e73460b702ee6e833c9de14a350f
stampaaaron <aaron@stampa.ch>
2024-09-30 12:56:06 +0000

    feat(frontend): setup tan stack router

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,15 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "@quassel/ui/style.css";
 import { ThemeProvider } from "@quassel/ui";
 import { ApiProvider } from "./provider/ApiProvider.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApiProvider>
       <ThemeProvider>
-        <App />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </ApiProvider>
   </StrictMode>
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,5 @@ export default defineConfig({
     port: 3001,
   },
   clearScreen: false,
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
 });
