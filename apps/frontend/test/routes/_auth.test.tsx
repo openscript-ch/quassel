dc20764aa2ecb96a915da206539ca4b1193c711c
Robin <git+r@obin.ch>
2024-10-24 02:53:06 +0000

    chore: enhance tests

--- apps/frontend/test/routes/_auth.test.tsx
+++ apps/frontend/test/routes/_auth.test.tsx
@@ -1,8 +1,9 @@
 import { describe, it, expect } from "vitest";
-import { render, waitFor } from "@testing-library/react";
+import { act, render, waitFor } from "@testing-library/react";
 import { createMemoryHistory, createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "../../src/routeTree.gen";
 import { ThemeProvider } from "@quassel/ui";
+import { $session } from "../../src/stores/session";
 
 describe("_auth route", () => {
   it("should redirect to /session if not logged in", async () => {
@@ -17,6 +18,29 @@ describe("_auth route", () => {
 
     await waitFor(() => {
       expect(history.location.pathname).toBe("/session");
+    });
+  });
+
+  it("should not redirect to /session if logged in", async () => {
+    const history = createMemoryHistory({ initialEntries: ["/"] });
+    const router = createRouter({ routeTree, history });
+
+    act(() => {
+      $session.set({ token: "token" });
+    });
+
+    render(
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    );
+
+    await waitFor(() => {
+      expect(history.location.pathname).toBe("/");
+    });
+
+    act(() => {
+      $session.set({});
     });
   });
 });
