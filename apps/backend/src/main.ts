3055d370681fbc441ae89b3313f12fe4b3a5dcf2
Robin <git+r@obin.ch>
2024-10-26 01:43:49 +0000

    chore: add validation pipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { version } from "../package.json";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder().setTitle("Quassel API").setVersion(version).setDescription("Gather language exposure data").build();
   const document = SwaggerModule.createDocument(app, config);
