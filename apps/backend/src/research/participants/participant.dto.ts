423a0d07d8db7c67ec3bae54a252704c489c3575
stampaaaron <aaron@stampa.ch>
2024-11-13 09:06:28 +0000

    fix: validating string date in request body

--- apps/backend/src/research/participants/participant.dto.ts
+++ apps/backend/src/research/participants/participant.dto.ts
@@ -1,13 +1,13 @@
 import { ApiProperty, OmitType, PartialType } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsDate, IsOptional } from "class-validator";
+import { IsDateString, IsOptional } from "class-validator";
 
 export class ParticipantDto {
   @ApiProperty({ example: 1, description: "The id of the participant (child id)" })
   id: number;
 
   @ApiProperty({ example: "2024-11-01T00:05:02.718Z", description: "The birthday of the participant" })
-  @IsDate()
+  @IsDateString()
   @IsOptional()
   birthday?: Date;
 
