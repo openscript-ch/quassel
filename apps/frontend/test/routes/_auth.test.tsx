abd84eede7e14885fb6b7f60d899d2b76e7a22c8
Robin <git+r@obin.ch>
2024-12-04 13:44:53 +0000

    chore: tune test

--- apps/frontend/test/routes/_auth.test.tsx
+++ apps/frontend/test/routes/_auth.test.tsx
@@ -1,24 +1,48 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { act, render, waitFor } from "@testing-library/react";
-import { createMemoryHistory, createRouter, RouterProvider } from "@tanstack/react-router";
-import { routeTree } from "../../src/routeTree.gen";
+import { createMemoryHistory, createRootRoute, createRoute, createRouter, RouterHistory, RouterProvider } from "@tanstack/react-router";
 import { ThemeProvider } from "@quassel/ui";
 import { $session } from "../../src/stores/session";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Route as AuthImport } from "../../src/routes/_auth";
+import { Route as SessionImport } from "../../src/routes/session";
 
 describe("_auth route", () => {
-  it("should redirect to /session if not logged in", async () => {
-    const history = createMemoryHistory({ initialEntries: ["/"] });
+  let history: RouterHistory;
+  let renderStage: () => void;
+
+  beforeEach(async () => {
+    history = createMemoryHistory({ initialEntries: ["/"] });
+    const rootRoute = createRootRoute();
+    const AuthRoute = AuthImport.update({
+      id: "/_auth",
+      getParentRoute: () => rootRoute,
+    } as never);
+    const SessionRoute = SessionImport.update({
+      id: "/session",
+      path: "/session",
+      getParentRoute: () => rootRoute,
+    } as never);
+    const IndexRoute = createRoute({ path: "/", component: () => <div>Index</div>, getParentRoute: () => AuthRoute });
+
+    const routeTree = rootRoute.addChildren([IndexRoute, AuthRoute, SessionRoute]);
     const queryClient = new QueryClient();
     const router = createRouter({ routeTree, history, context: { queryClient } });
 
-    render(
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </QueryClientProvider>
-    );
+    await router.load();
+
+    renderStage = () =>
+      render(
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <RouterProvider router={router as never} />
+          </ThemeProvider>
+        </QueryClientProvider>
+      );
+  });
+
+  it("should redirect to /session if not logged in", async () => {
+    renderStage();
 
     await waitFor(() => {
       expect(history.location.pathname).toBe("/session");
@@ -26,21 +50,11 @@ describe("_auth route", () => {
   });
 
   it("should not redirect to /session if logged in", async () => {
-    const history = createMemoryHistory({ initialEntries: ["/"] });
-    const queryClient = new QueryClient();
-    const router = createRouter({ routeTree, history, context: { queryClient } });
-
     act(() => {
       $session.set({ email: "hans.kanns@example.com" });
     });
 
-    render(
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </QueryClientProvider>
-    );
+    renderStage();
 
     await waitFor(() => {
       expect(history.location.pathname).toBe("/");
