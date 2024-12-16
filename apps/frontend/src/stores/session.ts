84170436494eb9ce7a208e8232f638467022744e
Robin <git+r@obin.ch>
2024-12-16 16:55:28 +0000

    chore: make session store more typesafe

--- apps/frontend/src/stores/session.ts
+++ apps/frontend/src/stores/session.ts
@@ -1,8 +1,6 @@
 import { persistentMap } from "@nanostores/persistent";
+import { components } from "../api.gen";
 
-type Session = {
-  email?: string;
-  role?: string;
-};
+type Session = Partial<Omit<components["schemas"]["SessionResponseDto"], "id">>;
 
 export const $session = persistentMap<Session>("session", {});
