6d61fef119d87b44f4ca53fb323913703a5d8187
stampaaaron <aaron@stampa.ch>
2025-01-08 08:57:02 +0000

    refactor: improve typing on useSort hook

--- apps/frontend/src/hooks/useSort.tsx
+++ apps/frontend/src/hooks/useSort.tsx
@@ -4,29 +4,34 @@ import { FileRoutesById } from "../routeTree.gen";
 
 export type SortOrder = "ASC" | "DESC";
 
-type RoutesWithSorting = "/_auth/administration/questionnaires/";
-
-export function useSort(route: FileRoutesById[RoutesWithSorting]) {
+export function useSort(route: FileRoutesById[keyof FileRoutesById]) {
   const s = route.useSearch();
-  const { sortBy, sortOrder } = s;
 
-  const ToggleLink = ({ sortKey }: { sortKey: typeof sortBy }) => (
-    <Link
-      to="."
-      search={(prev) => {
-        let sortOrder: SortOrder | undefined;
-        if (prev.sortBy !== sortKey) {
-          sortOrder = "ASC";
-        } else if (prev.sortOrder === "ASC") {
-          sortOrder = "DESC";
-        }
+  const sortBy = "sortBy" in s ? s.sortBy : undefined;
+  const sortOrder = "sortOrder" in s ? s.sortOrder : undefined;
 
-        return { ...prev, sortBy: sortOrder && sortKey, sortOrder };
-      }}
-    >
-      <SortToggle sortOrder={sortBy === sortKey ? sortOrder : undefined} />
-    </Link>
-  );
+  const ToggleLink = ({ sortKey }: { sortKey: typeof sortBy }) => {
+    return (
+      <Link
+        from={route.fullPath}
+        to="."
+        search={(prev) => {
+          if (!("sortBy" in prev)) return;
+
+          let sortOrder: SortOrder | undefined;
+          if (prev.sortBy !== sortKey) {
+            sortOrder = "ASC";
+          } else if (prev.sortOrder === "ASC") {
+            sortOrder = "DESC";
+          }
+
+          return { ...prev, sortBy: sortOrder && sortKey, sortOrder };
+        }}
+      >
+        <SortToggle sortOrder={sortBy === sortKey ? sortOrder : undefined} />
+      </Link>
+    );
+  };
 
   return { ToggleLink };
 }
