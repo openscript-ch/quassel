206248cd690c09d4167fe39a06e4a7e2319edec0
stampaaaron <aaron@stampa.ch>
2025-01-16 12:35:09 +0000

    feat: use mikro-orm for join query on csv export

--- apps/backend/src/system/export/export.service.ts
+++ apps/backend/src/system/export/export.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from "@nestjs/common";
 import { exec } from "node:child_process";
 import { promisify } from "node:util";
 import { ConfigService } from "../../config/config.service";
+import { EntityManager } from "@mikro-orm/postgresql";
+import { Entry } from "../../research/entries/entry.entity";
 
 const execPromise = promisify(exec);
 
 @Injectable()
 export class ExportService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly em: EntityManager
+  ) {}
 
   async fullDatabaseDump() {
     const user = this.configService.get("database.user");
@@ -28,9 +33,34 @@ export class ExportService {
     const host = this.configService.get("database.host");
     const port = this.configService.get("database.port");
 
-    const copyCommand = `
-        COPY (select * from entry) TO STDOUT WITH CSV HEADER DELIMITER ',';
-      `;
+    const query = this.em
+      .createQueryBuilder(Entry, "entry")
+      .select([
+        "*",
+        "carer.name as carer_name",
+        "entryLanguages.ratio",
+        "language.id as language_id",
+        "language.name as langauge_name",
+        "questionnaire.title as questionnaire_title",
+        "questionnaire.started_at as questionnaire_started_at",
+        "questionnaire.ended_at as questionnaire_ended_at",
+        "questionnaire.created_at as questionnaire_created_at",
+        "questionnaire.completed_at as questionnaire_completed_at",
+        "questionnaire.remark as questionnaire_remark",
+        "participant.id as participant_id",
+        "participant.birthday",
+        "study.id as study_id",
+        "study.title as study_title",
+      ])
+      .join("carer", "carer")
+      .join("entryLanguages", "entryLanguages")
+      .join("entryLanguages.language", "language")
+      .join("entry.questionnaire", "questionnaire")
+      .join("questionnaire.study", "study")
+      .join("questionnaire.participant", "participant")
+      .getQuery();
+
+    const copyCommand = `COPY (${query}) TO STDOUT WITH CSV HEADER DELIMITER ',';`;
 
     const command = `PGPASSWORD=${password} psql -U ${user} -h ${host} -p ${port} -d ${database} -c "${copyCommand}"`;
 
