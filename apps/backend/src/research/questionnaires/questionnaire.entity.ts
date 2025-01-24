ab8855aef008e9f13ebf5bf50551c8a6eb09ad54
stampaaaron <aaron@stampa.ch>
2025-01-24 13:38:54 +0000

    chore: add missing import

--- apps/backend/src/research/questionnaires/questionnaire.entity.ts
+++ apps/backend/src/research/questionnaires/questionnaire.entity.ts
@@ -1,4 +1,4 @@
-import { Collection, Entity, ManyToOne, OneToMany, Property } from "@mikro-orm/core";
+import { Collection, Entity, Formula, ManyToOne, OneToMany, Property } from "@mikro-orm/core";
 import { BaseEntity } from "../../common/entities/base.entity";
 import { Study } from "../studies/study.entity";
 import { Participant } from "../participants/participant.entity";
