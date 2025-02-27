bbd232fe971b613a2d360ed6fc4e6bb1806b5110
Yixuan Wu <y@ixuan.ch>
2025-02-27 12:23:22 +0000

    chore: configure server port to 3003

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        port: 3003,
+    }
+});
