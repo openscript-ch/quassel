a5de2238d50932729b820fd091c0d2867967fdba
Yixuan Wu <y@ixuan.ch>
2025-02-28 13:27:11 +0000

    chore: enhance Questionnaire layouts with Feature and Card components

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        port: 3003,
-    }
+  server: {
+      port: 3003,
+  }
 });
