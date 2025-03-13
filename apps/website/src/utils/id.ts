ca8bbf1106ef64c11918b44d80f3939240cd5153
Robin <git+r@obin.ch>
2025-03-13 19:56:05 +0000

    chore: clean up accordion

--- apps/website/src/utils/id.ts
+++
@@ -1,7 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 10);
-
-export function createId(name: string): string {
-  return `${name}-${nanoid()}`;
-}
