9df33ce4d7765fa6876ecd1d45638d9327aa1240
Robin <git+r@obin.ch>
2025-03-16 20:22:22 +0000

    chore: clean up features mapper

--- apps/website/src/mapper/features-mapper.ts
+++ apps/website/src/mapper/features-mapper.ts
@@ -1,6 +1,7 @@
 import type { ComponentProps } from "astro/types";
 import type Accordion from "../components/Accordion.astro";
 import type { SVGComponent } from "../types";
+import { getEntry, type DataEntryMap } from "astro:content";
 import IconCalendarWeek from "~icons/tabler/calendar-week";
 import IconCalendarPeriod from "~icons/tabler/calendar-clock";
 import IconDetectGaps from "~icons/tabler/arrow-autofit-width";
@@ -9,8 +10,6 @@ import IconStudyEntry from "~icons/tabler/report-search";
 import IconUsers from "~icons/tabler/users";
 import IconExportData from "~icons/tabler/file-export";
 import IconConsolidateData from "~icons/tabler/report-analytics";
-
-import { getEntry, type DataEntryMap } from "astro:content";
 
 type AccordionItemsProp = ComponentProps<typeof Accordion>["items"];
 
@@ -29,10 +28,8 @@ export async function mapFeatures<E extends keyof DataEntryMap["features"]>(id: 
   const features = (await getEntry("features", id))!.data;
 
   const items: AccordionItemsProp = features.map((feature) => ({
+    ...feature,
     icon: iconMapping[feature.icon],
-    title: feature.title,
-    description: feature.description,
-    screenshot: feature.screenshot,
   }));
 
   return items;
